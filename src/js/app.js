931ca56ffd3c0a7e2673b9759dab7498cdc60d05
korobprog <korobprog@gmail.com>
2023-08-02 20:20:39 +1000

    test lint

--- src/js/app.js
+++ src/js/app.js
@@ -6,21 +6,22 @@ let newBox;
 const attemptsLimit = 5;
 const goblin = new Goblin(attemptsLimit);
 const click = new ClickHandler('.playground');
+console.log('🚀 ~ file: app.js:9 ~ click:', click);
 const gameOn = setInterval(() => {
-   do {
-      newBox = Math.floor(Math.random() * 16) + 1;
-   } while (curBox === newBox);
-   try {
-      goblin.goblinOff(curBox);
-   } catch (err) {
-      if (err.message === 'Counter limitation') {
-         alert('GAME OVER!');
-         clearInterval(gameOn);
-         newBox = undefined;
-      }
-   }
-   goblin.goblinOn(newBox);
-   curBox = newBox;
+  do {
+    newBox = Math.floor(Math.random() * 16) + 1;
+  } while (curBox === newBox);
+  try {
+    goblin.goblinOff(curBox);
+  } catch (err) {
+    if (err.message === 'Counter limitation') {
+      alert('GAME OVER!');
+      clearInterval(gameOn);
+      newBox = undefined;
+    }
+  }
+  goblin.goblinOn(newBox);
+  curBox = newBox;
 }, 1000);
 
 gameOn();
