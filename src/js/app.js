a13dbe740145dfee392295419bffc7cef3b0744a
korobprog <korobprog@gmail.com>
2023-08-02 17:25:39 +1000

    games2

--- src/js/app.js
+++ src/js/app.js
@@ -6,20 +6,16 @@ let newBox;
 const attemptsLimit = 5;
 const goblin = new Goblin(attemptsLimit);
 const click = new ClickHandler('.playground');
-console.log(click);
 const gameOn = setInterval(() => {
    do {
       newBox = Math.floor(Math.random() * 16) + 1;
    } while (curBox === newBox);
-
    try {
       goblin.goblinOff(curBox);
    } catch (err) {
       if (err.message === 'Counter limitation') {
          alert('GAME OVER!');
          clearInterval(gameOn);
-         // удаление значения переменной для прекращения
-         // отображения Гоблина после сообщения "GAME OVER!"
          newBox = undefined;
       }
    }
